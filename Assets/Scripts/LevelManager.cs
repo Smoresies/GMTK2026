80418bdc39744cb70536cf2fb0c158645df97b2d
Oliver Mills <69769204+SllimRevilo@users.noreply.github.com>
2026-07-24 21:50:42 -0700

    level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,19 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> levels;
+    private GameObject currentLevel;
 
     void Awake()
     {
         RandomUtils.ShuffleList(levels, true);
+        SetNextRoom();
+    }
+
+    private void SetNextRoom()
+    {
+        Destroy(currentLevel);
+        GameObject nextRoom = levels[0];
+        levels.RemoveAt(0);
+        currentLevel = Instantiate(nextRoom, Vector3.zero, Quaternion.identity);
     }
 }
