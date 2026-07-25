99367a04295e3b3a0da9e91503452d624bf90d1d
Oliver Mills <69769204+SllimRevilo@users.noreply.github.com>
2026-07-25 09:32:32 -0700

    level manager works a bit better

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,21 +8,52 @@ public class LevelManager : MonoBehaviour
     [SerializeField]
     private ShopManager shopManager;
     [SerializeField]
-    private List<GameObject> levels;
-    private GameObject currentLevel;
+    private List<RoomPrefabWithWeight> listOfPrefabsWithWeights;
+    [SerializeField]
+    private GameObject firstRoomPrefab;
+    [SerializeField]
+    private GameObject lastRoomPrefab;
+    [SerializeField][Min(2)]
+    private int totalNumberOfRooms = 5;
+    private GameObject instantiatedLevel;
+    private Queue<GameObject> roomQueue = new Queue<GameObject>();
 
+    [Serializable]
+    public struct RoomPrefabWithWeight
+    {
+        public GameObject prefab;
+        public int weight;
+    }
     void Awake()
     {
-        RandomUtils.ShuffleList(levels, true);
+        int totalWeight = 0;
+        foreach (RoomPrefabWithWeight room in listOfPrefabsWithWeights)
+        {
+            totalWeight += room.weight;
+        }
+        roomQueue.Enqueue(firstRoomPrefab);
+        for (int i = 0; i < totalNumberOfRooms - 2; i++)
+        {
+            int randomWeight = UnityEngine.Random.Range(0, totalWeight);
+            foreach (RoomPrefabWithWeight room in listOfPrefabsWithWeights)
+            {
+                randomWeight -= room.weight;
+                if (randomWeight <= 0)
+                {
+                    roomQueue.Enqueue(room.prefab);
+                    break;
+                }
+            }
+        }
+        roomQueue.Enqueue(lastRoomPrefab);
         SetNextRoom();
     }
 
     private void SetNextRoom()
     {
-        Destroy(currentLevel);
-        GameObject nextRoom = levels[0];
-        levels.RemoveAt(0);
-        currentLevel = Instantiate(nextRoom, Vector3.zero, Quaternion.identity);
+        Destroy(instantiatedLevel);
+        GameObject nextRoom = roomQueue.Dequeue();
+        instantiatedLevel = Instantiate(nextRoom, Vector3.zero, Quaternion.identity);
     }
 
     private void CompleteLevel()
